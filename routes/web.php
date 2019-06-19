bae0d5ab1896dc77c0f849c19fe2ea8af6bed302
beekalam <beekalam@gmail.com>
2019-06-19 20:52:03 +0430

    08

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/threads','ThreadsController@index');
-Route::get('/threads/{thread}','ThreadsController@show');
-Route::post('/threads','ThreadsController@store');
-Route::post('/threads/{thread}/replies','RepliesController@store');
-
 Auth::routes();
 
+// Route::get('/threads','ThreadsController@index');
+// Route::get('/threads/{thread}','ThreadsController@show');
+// Route::get('/threads/create','ThreadsController@create');
+// Route::post('/threads','ThreadsController@store');
+
+Route::resource('threads','ThreadsController');
+
+Route::post('/threads/{thread}/replies','RepliesController@store');
 Route::get('/home', 'HomeController@index')->name('home');
