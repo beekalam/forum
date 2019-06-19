b44fd615a376351a86e50220c67321ad3b5a45db
beekalam <beekalam@gmail.com>
2019-06-19 17:39:02 +0430

    03

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -2,16 +2,38 @@
 
 @section('content')
     <div class="container">
+
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
                     <div class="card-header">{{ $thread->title }}</div>
 
                     <div class="card-body">
-                       {{ $thread->body }}
+                        {{ $thread->body }}
                     </div>
+
                 </div>
             </div>
         </div>
+
+
+        <div class="row justify-content-center" style="margin-top:8px;">
+            <div class="col-md-8">
+                @foreach($thread->replies as $reply)
+                    <div class="card" style="margin-top:8px">
+                        <div class="card-header">
+                            <a href="#">
+                                {{ $reply->owner->name }}
+                            </a>
+                            said {{ $reply->created_at->diffForHumans() }} ...
+                        </div>
+                        <div class="card-body">
+                            {{ $reply->body }}
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+
     </div>
 @endsection
